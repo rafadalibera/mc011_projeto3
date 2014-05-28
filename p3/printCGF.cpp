aae3d6018eb41e05640059fa3bd66f0b0e9515f4
cicero <cicerolz@gmail.com>
2014-05-28 14:48:49 -0300

    Includes funcionam

--- p3/printCGF.cpp
+++ p3/printCGF.cpp
@@ -1,20 +1,44 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/IR/User.h"
+#include "llvm/IR/Instructions.h"
+#include <set>
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/CFG.h"
+#include <set>
 
 using namespace llvm;
 
 namespace {
-  struct printCFG : public FunctionPass {
-    static char ID;
-    printCFG() : FunctionPass(ID) {}
+	struct printCFG : public FunctionPass {
+		static char ID;
+		printCFG() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-	  F.viewCFG(); 
-      return false;
-    }
-  };
+		virtual bool runOnFunction(Function &F) {
+			F.viewCFG();
+			return false;
+		}
+	};
+	class genKill
+	{
+	public:
+		std::set<Instruction *>gen;
+		std::set<Instruction *>kill;
+	};
+
+	class beforeAfter {
+	public:
+		std::set<Instruction*> before;
+		std::set<Instruction*> after;
+	};
 }
-
 char printCFG::ID = 0;
 static RegisterPass<printCFG> X("printCFG", "Print CFG Pass", false, false);
